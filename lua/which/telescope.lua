156c41ca981b141ac9775ed45be5926f0bb1c31f
Chris <skylake112@outlook.com>
2022-04-26 16:23:48 -0600

    Touch up some which-key keymaps for trouble and telescope

--- lua/which/telescope.lua
+++ lua/which/telescope.lua
@@ -1,7 +1,6 @@
 require("which-key").register({
   a = { "<cmd>lua require('telescope.builtin').find_files({ hidden = true })<CR>", "Find hidden files" },
   b = { "<cmd>Telescope buffers<cr>" },
-
   c = { "<cmd>lua require('telescope.builtin.internal').colorscheme({enable_preview = true})<cr>", "Colorscheme with Preview" },
   C = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
 
@@ -9,10 +8,8 @@ require("which-key").register({
 
   f = { "<cmd>Telescope find_files<cr>", "Find files" },
   F = { "<cmd>Telescope live_grep<cr>", "Grep files" },
-
   h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
   H = { "<cmd>Telescope heading<cr>", "Find Header" },
-
   i = { "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>", "Media" },
 
   j = { "<cmd>lua require('harpoon.ui').nav_file(1)<cr>", "Open File 1" },
@@ -24,20 +21,16 @@ require("which-key").register({
   U = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
 
   L = { "<cmd>Telescope resume<cr>", "Last Search" },
-
   M = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "Harpoon menu" },
   m = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "Add file" },
-
   p = { "<cmd>Telescope projects<cr>", "Projects" },
 
   r = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
   R = { "<cmd>Telescope registers<cr>", "Registers" },
-
   s = { "<cmd>Telescope grep_string<cr>", "Text under cursor" },
   S = { "<cmd>Telescope symbols<cr>", "Search symbols" },
 
   t = { "<cmd>Telescope live_grep<cr>", "Text" },
-
   z = { "<cmd>Telescope zoxide list<CR>", "Zoxide" },
 
 }, { prefix = ";", mode = "n", noremap = true })
--- lua/which/trouble.lua
+++ lua/which/trouble.lua
@@ -1,12 +1,13 @@
 require("which-key").register({
 
-  a = { "<cmd>Trouble<cr>", "Trouble" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Document Diagnostic" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
-  l = { "<cmd>Trouble loclist<cr>", "Loclist" },
-  L = { "<cmd>Trouble lsp_references<cr>", "Lsp References" },
-  q = { "<cmd>Trouble quickfix<cr>", "Quickfix" },
-  t = { "<cmd>TodoTrouble<cr>", "Todos" },
-  x = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace diagnostics" }
+  a = { "<cmd>TroubleToggle<cr>", "Trouble" },
+  d = { "<cmd>TroubleToggle lsp_type_definitions", "Type definitions" },
+  j = { "<cmd>TroubleToggle document_diagnostics<cr>", "Document Diagnostic" },
+  k = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  l = { "<cmd>TroubleToggle loclist<cr>", "Loclist" },
+  L = { "<cmd>TroubleToggle lsp_references<cr>", "Lsp References" },
+  [";"] = { "<cmd>TroubleToggle lsp_definitions<cr>", "Lsp Definitions" },
+  q = { "<cmd>TroubleToggle quickfix<cr>", "Quickfix" },
+  t = { "<cmd>TroubleToggle<cr>", "Trouble" },
 
 }, { prefix = "t", mode = "n", noremap = true, silent = true })
